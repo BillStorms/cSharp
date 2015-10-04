d589ee946499973534e5b5669c1670c3db419664
billstorms1 <bstorms@cafepress.com>
2015-10-03 21:41:44 -0400

    A program to add the value of all the prime numbers up to 1000.  Challenge for codeeval.com  So far, fail

---
+++ SumofPrimeNumbersChallenge/SumofPrimeNumbersChallenge/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumofPrimeNumbersChallenge
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            string num1String;
+
+            int num1;
+            List<string> primeNumbers = new List<string>();
+
+            for (int i = 2; i < 1000; i++)
+            {
+                num1 = i;
+
+                {
+                    for (int a = 2; a <= num1/2; a++)
+                    {
+                        if (num1%a == 0)
+                        {
+                            return;
+                        }
+                        num1String = num1.ToString();
+                        primeNumbers.Add(num1String);
+                    }
+                }
+            }
+
+            Console.WriteLine(primeNumbers);
+            Console.ReadLine();
+        }
+    }   
+}
+
+
+
+
+
