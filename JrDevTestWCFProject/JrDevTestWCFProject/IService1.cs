813bd23eed2979ec7dd7e6f3e0fee91421378853
billstorms1 <bstorms@cafepress.com>
2015-10-22 23:17:55 -0400

    JrDevWCFProject

---
+++ JrDevTestWCFProject/JrDevTestWCFProject/IService1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+
+namespace JrDevTestWCFProject
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string DisplayProjectName();
+
+        [OperationContract]
+        string[] DisplayCurrentDateTime();
+
+        [OperationContract]
+        string EchoInput(int input);
+
+    }
+}
---
+++ JrDevTestWCFProject/JrDevTestWCFProject/Service1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+
+namespace JrDevTestWCFProject 
+{
+    public class Service1 : IService1
+    {
+        public string DisplayProjectName()
+        {
+            var name = Path.GetFileName(Assembly.GetExecutingAssembly().FullName);
+            var version = Path.GetFileName(Assembly.GetExecutingAssembly().ImageRuntimeVersion);
+            var result =  string.Format("Assembly Name : {0}  Version Number : {1}",name, version);
+            return result;
+        }
+        
+        public string[] DisplayCurrentDateTime()
+        {
+            DateTime serverTime = DateTime.Now;
+            DateTime gmtTime = DateTime.UtcNow;
+
+            var serverTimeString = serverTime.ToString();
+            var gmtTimeString = gmtTime.ToString();
+            var span = gmtTime - serverTime;
+            var diff = (int) span.TotalHours;
+            var diffString = String.Format("The time difference between GMT and server time is {0} hours.", diff);
+
+          //  List<string> resultList = new List<string>{serverTime.ToString(), gmtTime.ToString(), diffString};
+            
+            string[] resultList = new string[3];
+            resultList[0] = serverTimeString;
+            resultList[1] = gmtTimeString;
+            resultList[2] = diffString;
+            
+            return resultList;
+        }
+
+        public string EchoInput(int input)
+        {
+            return input.ToString();
+        }
+    }
+}
